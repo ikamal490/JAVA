319614cd0b0b7ee744a28d85a1cd7c2e1c375d13
Kamal Singh Dhakad <kdhakad602@gmail.com>
2024-12-04 11:20:07 +0530

    Print natural numbers :

---
+++ _22_do_while_loop/problem.java
@@ -0,0 +1,15 @@
+package _22_do_while_loop;
+
+public class problem {
+    public static void main(String[] args) {
+        /*
+            Write a program to print natural numbers from 50 to 151 by using of do - while loop
+         */
+
+        int i = 50;
+        do{
+            System.out.println(i);
+            i++;
+        } while(i<=151);
+    }
+}
