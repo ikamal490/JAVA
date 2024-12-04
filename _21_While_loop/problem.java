632b10fab5d6d2add0c14d0874a77a0b61e20051
Kamal Singh Dhakad <kdhakad602@gmail.com>
2024-12-04 10:26:38 +0530

    While loop

---
+++ _21_While_loop/problem.java
@@ -0,0 +1,14 @@
+package _21_While_loop;
+
+public class problem {
+    public static void main(String[]args) {
+        /*
+            Write a java program to print natural numbers from 100 to 200
+         */
+        int i = 100;
+        while(i<=200){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
---
+++ _21_While_loop/while_loop.java
@@ -0,0 +1,20 @@
+package _21_While_loop;
+
+import java.sql.SQLOutput;
+
+public class while_loop {
+    public static void main(String[] args) {
+        System.out.println("\nPrint Number form 1 to 50 Using while loop :");
+        int i = 1;
+        while (i <= 50) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Loop has been ended");
+//        System.out.println("For infinite Loop: ");
+//        while (true) {
+//            System.out.println("I am kamal ");
+//        }
+    }
+    }
+
