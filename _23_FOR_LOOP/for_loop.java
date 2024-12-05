23055756acfd9baf02db345b4f99d862c67bfb15
Kamal Singh Dhakad <kdhakad602@gmail.com>
2024-12-05 21:21:28 +0530

    For Loop

---
+++ _23_FOR_LOOP/for_loop.java
@@ -0,0 +1,18 @@
+package _23_FOR_LOOP;
+
+public class for_loop {
+    public static void main(String[]args){
+        for (int i = 1;  i<=10; i++){
+            System.out.println(i);
+        }
+
+        System.out.println();
+        // Decrement in the Loop :
+
+        for(int i = 10; i>0; i--){
+            System.out.println(i);
+        }
+
+
+    }
+}
