14128a73e28429acc3218d0e405bdb7a932f1d42
Kamal Singh Dhakad <kdhakad602@gmail.com>
2024-12-10 03:01:32 +0530

    Method without using of static obj

---
+++ _31_Methods/Calling_a_Method.java
@@ -0,0 +1,28 @@
+package _31_Methods;
+
+public class Calling_a_Method {
+     int logic (int x, int y){
+        int z;
+        if(x<y){
+            z=x+y;
+        }
+        else{
+            z=(x+y)*10;
+        }
+        return z;
+    }
+    public static void main(String[]args){
+        int a=14;
+        int b=4;
+        int c;
+        // Method invocation using Object creation
+        Calling_a_Method obj = new Calling_a_Method(); // We create an object of class name because we don't use of static keyboard
+        c = obj.logic(a,b);
+        int a1=5;
+        int b1=43;
+        int c1;
+        c1 = obj.logic(a1,b1);
+        System.out.println(c );
+        System.out.println(c1);
+    }
+}
