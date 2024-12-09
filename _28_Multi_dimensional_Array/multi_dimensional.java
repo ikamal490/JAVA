6b4629e6bb1309caac5b09e7fcbddaf9349f8529
Kamal Singh Dhakad <kdhakad602@gmail.com>
2024-12-09 10:42:12 +0530

    2-D or 3-D Arrays

---
+++ _28_Multi_dimensional_Array/multi_dimensional.java
@@ -0,0 +1,26 @@
+package _28_Multi_dimensional_Array;
+
+public class multi_dimensional {
+    public static void main(String[]args){
+        int [] marks ;// It is a  1-D Array
+        String [][][] arr = new String[2][3][4]; // It is a  3-D Array
+        int [][] flats ; // It is a  2-D Array
+        flats = new int[2][3];
+        flats[0][0] = 101;
+        flats[0][1] = 102;
+        flats[0][2] = 103;
+        flats[1][0] = 201;
+        flats[1][1] = 202;
+        flats[1][2] = 203;
+
+        // Displaying the 2-D Array (for loop )
+        System.out.println("Printing a 2-D Array using for loop");
+        for(int i=0; i<flats.length; i++ ){
+            for(int j=0; j<flats[i].length; j++){
+                System.out.print(flats[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
