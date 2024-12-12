add821ccdf4f61ad3e0928c51fcb541a55b87000
Kamal Singh Dhakad <kdhakad602@gmail.com>
2024-12-12 19:34:39 +0530

    To check it is int/float ...

---
+++ _05User_Define/hasNext.java
@@ -0,0 +1,15 @@
+package _05User_Define;
+import java.util.Scanner;
+
+public class hasNext {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value :");
+        boolean a1 = sc.hasNextInt();
+        System.out.println(a1);
+
+
+
+    }
+
+}
