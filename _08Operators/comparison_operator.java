81cf5446b30c1badd5ae31966f8d906942eba0ec
Kamal Singh Dhakad <kdhakad602@gmail.com>
2024-12-19 08:56:23 +0530

    comparisons operators

---
+++ _08Operators/comparison_operator.java
@@ -0,0 +1,21 @@
+package _08Operators;
+
+public class comparison_operator {
+    /*
+            == : Equal to
+            != : Not equal to
+            < : Less than
+            > : Greater than
+            <= : Less than or equal to
+            >= : Greater than or equal to
+     */
+    public static void main(String[]args){
+        System.out.println(64==32); // return false
+        System.out.println(23!=45); //return true
+        System.out.println(34!=34); // return false
+        System.out.println(73>=23); // return true
+        System.out.println(34<=89); // return true
+        System.out.println(53>23);  // return true
+        System.out.println(53<23); // return false
+    }
+}
