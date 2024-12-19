a1e0b062d9a3b1fdb96eeb0e10ae23d06ea63aff
Kamal Singh Dhakad <kdhakad602@gmail.com>
2024-12-19 08:56:02 +0530

    OR || operator

---
+++ _08Operators/or_operator.java
@@ -0,0 +1,11 @@
+package _08Operators;
+/*
+    Or (||) operator
+    Agar ek bhi condition true hoti hai toh true return karega
+ */
+public class or_operator {
+    public static void main(String[] args) {
+        System.out.println(34 >= 23 || 45 > 23);  // It give true
+        System.out.println(45 <= 23 || 67 > 23); // It give true
+    }
+}
