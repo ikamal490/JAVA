aea310de8d5a593958a15266430bee8cc43fe346
Kamal Singh Dhakad <kdhakad602@gmail.com>
2024-12-19 08:57:24 +0530

    And && operator

---
+++ _08Operators/and_operator01.java
@@ -0,0 +1,12 @@
+package _08Operators;
+/*
+    AND(&&) operator
+    Agar ek bhi condition false hoti hai toh false return karega
+ */
+public class and_operator01 {
+    public static void main(String[]args){
+        System.out.println(34>=23 && 45>23);  // It give true
+        System.out.println(45<=23 && 67>23); // It give False
+
+    }
+}
