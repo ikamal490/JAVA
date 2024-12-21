dc0e1abd0a742d5cd675087c4a25893c6dc55b98
Kamal Singh Dhakad <kdhakad602@gmail.com>
2024-12-21 22:06:25 +0530

    Adding of numbers

---
+++ _03_Variables_and_Data_types/sum.java
@@ -0,0 +1,17 @@
+package _03_Variables_and_Data_types;
+
+public class sum {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 10;
+        int c = 10;
+        int d = 12;
+        int result = a+b+c+d;
+        System.out.println(result);
+        System.out.println("The sum of these numbers is :" +result); // we can also use this type
+
+        // Direct method
+        System.out.println("The sum is :" +(30+10+10+12));
+        System.out.println(30+10+10+12);
+    }
+}
