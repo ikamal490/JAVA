8840084c6b89dfa089e445fa95aca224d02cdaa8
Kamal Singh Dhakad <kdhakad602@gmail.com>
2024-12-21 22:13:46 +0530

    They are .class files

---
+++ Self_Practise/sum.java
@@ -0,0 +1,13 @@
+package Self_Practise;
+
+public class sum {
+    public static void main(String[]args){
+        int a = 3;
+        int b = 23;
+        int c = 34;
+        int d = 55;
+        int add = a+b+c+d;
+        System.out.println("The sum of these numbers is :" +add);
+        System.out.println(add);
+    }
+}
