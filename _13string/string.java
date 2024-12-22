1814d72de67eb42cafba5f6c378028718c86df18
Kamal Singh Dhakad <kdhakad602@gmail.com>
2024-12-22 19:28:22 +0530

    print the string

--- _13string/string.java
+++ _13string/string.java
@@ -3,27 +3,42 @@ import java.util.Scanner;
 /*
        -->  A String is a sequence of characters
        --> A string is a class but can be used like a data type
-
  */
 public class string {
     public static void main(String[] args) {
+        //String name = new String("Kamal");  -- It is boring
 
-    //    String name = new String("Kamal");
-    //    String name = "Kamal";  // java give Special Support  to String we can also write this type
-    //    System.out.print("What is your name : ");
-    //    System.out.print(name);
+        // Normal Way
+        String name = "Kamal";  // java give Special Support  to String we can also write this type
+        System.out.print("What is your name :");
+        System.out.println(name);
+        /*
+         If you want output like that use above code :
+         What is your name : Kamal
+         */
+        String name1 = "Kamal";
+        System.out.println("What is your name :");
+        System.out.println(name1);
+         /*
+         If you want output like that use above code :
+         What is your name :
+         Kamal
+         */
 
-        int a = 7;
-        float b = 3.3443f;
-    //    System.out.printf("The value of a is %d and the value of b is %f ", a, b);
-    //    System.out.format("The value of a is %d and the value of b is %f ", a, b);   // This is same like a printf
+        // Input Type
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter you NAAM :");
+        String NAAM = sc.nextLine();
+        System.out.println(NAAM);
 
-            Scanner sc = new Scanner(System.in);
-     //       String st = sc.next();  // Use for print only one word of the String
-     //       System.out.println(st);
-            String st = sc.nextLine();   // Use for print whole sentence
-            System.out.println(st);
+        // Concept for print full name i.e kamal singh dhakad
+        System.out.println("Enter the Full name :");
+        String line = sc.nextLine();
+        System.out.println(line);
 
-
+        // Concept for print only name i.e kamal
+        System.out.println("Enter only name :");
+        String single_line = sc.next();  // Use for print only one word of the String
+        System.out.println(single_line);
     }
 }
