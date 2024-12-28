b2129be378c85cb2317922489c06cfc39f23f6aa
Kamal Singh Dhakad <kdhakad602@gmail.com>
2024-12-28 22:32:32 +0530

    Declaration and then Memory allocation

---
+++ _26_Array/way_2.java
@@ -0,0 +1,14 @@
+package _26_Array;
+// DataType[] ArrName -- Format of an array
+public class way_2 {
+    public static void main(String[] args) {
+        // Declaration and then Memory allocation
+        int[] marks;
+        marks = new int[60];
+        marks[0] = 89;
+        marks[1] = 88;
+        marks[35] = 94;
+        marks[23] = 43;
+        System.out.println(marks[23]);
+    }
+}
