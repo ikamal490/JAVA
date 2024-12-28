758191c5a915bc3c74ee48654f565401de0a710d
Kamal Singh Dhakad <kdhakad602@gmail.com>
2024-12-28 22:32:45 +0530

    Declaration + Initialization

---
+++ _26_Array/way_3.java
@@ -0,0 +1,9 @@
+package _26_Array;
+// DataType[] ArrName ---- Format
+public class way_3 {
+    public static void main(String[] args) {
+        // Declaration + Initialization
+        int[] marks = {34,45,73,82,39};
+        System.out.println(marks[4]);
+    }
+}
