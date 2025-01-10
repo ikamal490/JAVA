de4a22e58b27fd514bb7dc9d074cedffd4d26ff6
Kamal Singh Dhakad <kdhakad602@gmail.com>
2025-01-10 16:38:37 +0530

    Packages

---
+++ _61_to_65_Packages_/Packages.java
@@ -0,0 +1,14 @@
+package _61_to_65_Packages_;
+//import java.util.Scanner;
+import java.util.*; // It imports all the classes
+public class Packages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        // It's another method for without use of import java.util.Scanner;
+        System.out.println("Enter the value :");
+        int a = scanner.nextInt();
+        System.out.println("The value of a = " + a);
+
+    }
+}
