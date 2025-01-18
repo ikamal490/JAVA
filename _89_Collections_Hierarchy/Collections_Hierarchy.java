61404b75948ae6bb7b46752b10a513f1a818e28c
Kamal Singh Dhakad <kdhakad602@gmail.com>
2025-01-18 06:24:29 +0530

    Push mt kar esko

--- _89_Collections_Hierarchy/Collections_Hierarchy.java
+++ _89_Collections_Hierarchy/Collections_Hierarchy.java
@@ -1,13 +1,9 @@
 package _89_Collections_Hierarchy;
 
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.TreeSet;
-
 public class Collections_Hierarchy {
     public static void main(String[] args) {
-        ArrayList;  // Throw error
-        Set; // Throw error
-        TreeSet; // Throw error
+//        ArrayList;  // Throw error
+//        Set; // Throw error
+//        TreeSet; // Throw error
     }
 }
