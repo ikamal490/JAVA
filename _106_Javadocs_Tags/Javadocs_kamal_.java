57a6f36ac4201846a0a68faf42fbc389e474521e
Kamal Singh Dhakad <kdhakad602@gmail.com>
2025-01-19 15:57:24 +0530

    Javadoc

---
+++ _106_Javadocs_Tags/Javadocs_kamal_.java
@@ -0,0 +1,17 @@
+package _106_Javadocs_Tags;
+
+ /**
+ * @author Kamal (Kya Gunda banega re tu )
+ *  @version 2.1
+ * @since  2005
+ * @see <a href= " https://docs.oracle.com/en/java/javase/23/docs/api/index.html">Java Docs </a>
+ */
+
+class Javadocs_kamal_ {
+    static void add(int a, int b){
+        System.out.println("The sum is : " +a+b);
+    }
+    public static void main(String[] args) {
+        System.out.println("This is my main method ");
+    }
+}
