e1fe68964d6d8f7756d993eb10b9fdd781d3753a
Kamal Singh Dhakad <kdhakad602@gmail.com>
2025-01-21 22:35:19 +0530

    Print the elements using for-each

---
+++ MyPractice_code/_032_.java
@@ -0,0 +1,12 @@
+package MyPractice_code;
+/*
+    Print the elements of an array using a for-each loop.
+ */
+public class _032_ {
+    public static void main(String[] args) {
+        int[] numbers = {23,43,89,39,73,73};
+        for(int element : numbers){
+            System.out.println(element);
+        }
+    }
+}
