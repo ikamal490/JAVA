1fd6142552dd98f0f3473a9f9dabb83a40eb3685
Kamal Singh Dhakad <kdhakad602@gmail.com>
2025-01-21 22:34:38 +0530

    multiplication table using do-while

---
+++ MyPractice_code/_031_.java
@@ -0,0 +1,13 @@
+package MyPractice_code;
+/*
+    Implement a program that prints the multiplication table of 5 using a do-while loop.
+ */
+public class _031_ {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println(5*i);
+            i++;
+        }while(i<=10);
+    }
+}
