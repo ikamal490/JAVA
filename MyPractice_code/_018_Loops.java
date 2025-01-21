8259ca00e8c4203ba1c13015cf9f387b88318616
Kamal Singh Dhakad <kdhakad602@gmail.com>
2025-01-21 22:30:12 +0530

    Print Odd Number

---
+++ MyPractice_code/_018_Loops.java
@@ -0,0 +1,14 @@
+package MyPractice_code;
+/*
+Print Odd Number
+    Print odd number using For loop
+    WAP to print first n odd numbers using a for loop.
+    Let's print odd numbers from 1 to 100
+ */
+public class _018_Loops {
+    public static void main(String[] args) {
+        for(int i=0; i<=100; i++){
+            System.out.println((2*i+1));
+        }
+    }
+}
