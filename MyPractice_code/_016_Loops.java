0e186a25dcbc05424ec01d61ea28f23576c16e1e
Kamal Singh Dhakad <kdhakad602@gmail.com>
2025-01-21 22:29:48 +0530

    Print Natural Numbers

---
+++ MyPractice_code/_016_Loops.java
@@ -0,0 +1,15 @@
+package MyPractice_code;
+/*
+Print Natural Numbers
+    Print natural number using while loop
+    WAP to print natural numbers from 100 to 200.
+ */
+public class _016_Loops {
+    public static void main(String[] args) {
+        int i=100;
+        while(i<=200){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
---
+++ MyPractice_code/_017_Loops.java
@@ -0,0 +1,15 @@
+package MyPractice_code;
+/*
+Print Natural Numbers
+    Print natural number using do-while loop
+    WAP to print natural numbers from 100 to 200.
+ */
+public class _017_Loops {
+    public static void main(String[] args) {
+        int i=100;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=200);
+    }
+}
