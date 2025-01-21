93abe8e1c8880893bb40a08bae58a1b4de1fc8c9
Kamal Singh Dhakad <kdhakad602@gmail.com>
2025-01-21 22:33:27 +0530

    Print Numbers

---
+++ MyPractice_code/_028_Loops_Easy_type.java
@@ -0,0 +1,11 @@
+package MyPractice_code;
+/*
+    Write a program to print numbers from 1 to 10 using a for loop
+ */
+public class _028_Loops_Easy_type {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
