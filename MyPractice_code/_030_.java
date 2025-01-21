1638106a52b0d4667990fd5344bc4ea2dcd07f0e
Kamal Singh Dhakad <kdhakad602@gmail.com>
2025-01-21 22:34:09 +0530

    display all even numbers

---
+++ MyPractice_code/_030_.java
@@ -0,0 +1,11 @@
+package MyPractice_code;
+/*
+    Write a program to display all even numbers between 1 and 20 using a for loop.
+ */
+public class _030_ {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            System.out.println(2*i);
+        }
+    }
+}
