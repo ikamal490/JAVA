da8e4c6be98b00903ea756b09616f702f50c7a71
Kamal Singh Dhakad <kdhakad602@gmail.com>
2025-01-22 17:22:36 +0530

    Finding the Largest of Three Numbers Using Conditional Operators:

---
+++ MyPractice_code/_064_Operator.java
@@ -0,0 +1,44 @@
+package MyPractice_code;
+
+import java.util.Scanner;
+
+/*
+Finding the Largest of Three Numbers Using Conditional Operators:
+     Write a Java program that takes three numbers as input and prints
+      the largest one using the conditional (ternary) operator.
+
+      condition ? expression1 : expression2;  ---> conditional (ternary) operator.
+
+ */
+public class _064_Operator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        System.out.println("Enter a number 1 ");
+//        int a = sc.nextInt();
+//        System.out.println("Enter a number 2 ");
+//        int b = sc.nextInt();
+//        System.out.println("Enter a number 3 ");
+//        int c = sc.nextInt();
+
+//        int largest = (a > b) ? (a > c ? a : c) : (b > c ? b : c); // difficult
+//        System.out.println("The largest number is: " + largest);
+
+
+        // By using of if-else :
+//        System.out.println("Enter a number 1 ");
+//        int a1 = sc.nextInt();
+//        System.out.println("Enter a number 2 ");
+//        int b1 = sc.nextInt();
+//        System.out.println("Enter a number 3 ");
+//        int c1 = sc.nextInt();
+//        if(a1>b1 && b1>c1){
+//            System.out.println(a1+ " is the largest number");
+//        } else if (b1>a1 && a1>c1) {
+//            System.out.println(b1+ " is the largest number");
+//        }
+//        else{
+//            System.out.println(c1+ " is the largest number");
+//        }
+    }
+}
