fa7d4189c0347d699844316cc053c4f01d67975d
Kamal Singh Dhakad <kdhakad602@gmail.com>
2025-01-22 14:47:03 +0530

    Print Variables

---
+++ MyPractice_code/_055_Easy_Type.java
@@ -0,0 +1,27 @@
+package MyPractice_code;
+/*
+Print Variables
+    Write a Java program to declare variables of all primitive data types (int, float, double, char, boolean, etc.),
+    assign values to them, and print their values.
+    Example Output:
+    int: 10
+    float: 5.5
+    double: 10.99
+    char: A
+    boolean: true
+ */
+public class _055_Easy_Type {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 5.5F;
+        double c = 10.99D;
+        char d = 'A';
+        boolean e = 4<9;
+
+        System.out.println("int: " + a);
+        System.out.println("float: " + b);
+        System.out.println("double: " + c);
+        System.out.println("char: " + d);
+        System.out.println("boolean: " + e);
+    }
+}
