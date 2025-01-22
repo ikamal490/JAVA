c73d0322aecc307ee48477ce22c3060c72710b5e
Kamal Singh Dhakad <kdhakad602@gmail.com>
2025-01-22 17:17:55 +0530

    WAP to reverse an array

---
+++ MyPractice_code/_044_Array.java
@@ -0,0 +1,12 @@
+package MyPractice_code;
+/*
+    Problem: WAP to reverse an array
+ */
+public class _044_Array {
+    public static void main(String[] args) {
+        int[] values = {54,98,79,56,99,34,546};
+        for(int i=values.length-1; i>=0; i--){
+            System.out.println(values[i]);
+        }
+    }
+}
