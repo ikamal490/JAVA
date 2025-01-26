d31368aacf77762707033f3f85202f7043b13600
Kamal Singh Dhakad <kdhakad602@gmail.com>
2025-01-26 17:21:12 +0530

    Java Loops I

---
+++ Hacker_Rank_Questions/Question02.java
@@ -0,0 +1,27 @@
+package Hacker_Rank_Questions;
+// Java Loops I
+import java.util.Scanner;
+
+/*
+Sample Input:
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+ */
+public class Question02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.format("%d x %d = %d\n", n, i, n * i);
+        }
+    }
+}
+
