0ebe35415b69841722f28604cb414260b7692fac
Kamal Singh Dhakad <kdhakad602@gmail.com>
2025-01-26 17:22:49 +0530

    Java Int to String

---
+++ Hacker_Rank_Questions/Question05.java
@@ -0,0 +1,20 @@
+package Hacker_Rank_Questions;
+
+import java.util.Scanner;
+
+// Java Int to String
+public class Question05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+//        String str = String.valueOf(num);
+//        System.out.println(str);
+        System.out.println(String.valueOf("Good job"));
+    }
+}
+/*
+Output:
+100
+Good job
+
+ */
